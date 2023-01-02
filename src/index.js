b86cb1b043ea722178707d09b10794cc814445f1
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-01-02 10:11:33 -0800

    commit tree bd5fd4f4f4d3bd1059d3a6edd6f48f0c79341228

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
+//Selecting event target via input element
 const init = () => {
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+/* 
+        if (input.value > input.length()) {
+            alert('Please request a valid ID!');
+        } */
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p')
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+    });
+  }
+
+
+
+//Selecting event target fromm the array like object
+/* const init = () => {
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log(event.target.children[1].value);
+    })
   
 }
-
+ */
 document.addEventListener('DOMContentLoaded', init);
